cefba16645d7d8f03bd02439be94cf53c16ce4ac
Michael Fridman <mf192@icloud.com>
2022-04-24 00:13:22 -0400

    Add clickhouse tests

---
+++ tests/clickhouse/clickhouse_test.go
@@ -0,0 +1,42 @@
+package clickhouse_test
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/matryer/is"
+	"github.com/pressly/goose/v3"
+	"github.com/pressly/goose/v3/internal/testdb"
+)
+
+func TestClickHouse(t *testing.T) {
+	t.Parallel()
+	is := is.New(t)
+
+	migrationDir := filepath.Join("testdata", "migrations")
+	db, cleanup, err := testdb.NewClickHouse()
+	is.NoErr(err)
+	t.Cleanup(cleanup)
+
+	goose.SetDialect("clickhouse")
+
+	currentVersion, err := goose.GetDBVersion(db)
+	is.NoErr(err)
+	is.Equal(currentVersion, int64(0))
+
+	err = goose.Up(db, migrationDir)
+	is.NoErr(err)
+
+	currentVersion, err = goose.GetDBVersion(db)
+	is.NoErr(err)
+	is.Equal(currentVersion, int64(1))
+
+	err = goose.DownTo(db, migrationDir, 0)
+	is.NoErr(err)
+	// TODO(mf): this will fail if SETTINGS mutations_sync = 0.. because the
+	// delete operation above is async and that is the default. I updated the
+	// dialect string, but not sure that's correct..
+	currentVersion, err = goose.GetDBVersion(db)
+	is.NoErr(err)
+	is.Equal(currentVersion, int64(0))
+}
